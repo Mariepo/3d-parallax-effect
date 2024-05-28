eaef9dc43dcf6dbdf3dec14703b1b7d6604c23f5
Mariepo <marie.potte.62@gmail.com>
2024-05-28 17:35:29 +0200

    ajout de l'animation reveal au chargement de la page

--- js/index.js
+++ js/index.js
@@ -26,6 +26,7 @@ function update(cursorPosition){
 update(0);
 
 window.addEventListener("mousemove", (e)=>{
+    if(timeline.isActive()) return;
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY  - window.innerHeight / 2;
 
@@ -33,3 +34,44 @@ window.addEventListener("mousemove", (e)=>{
     update(e.clientX);
 }); 
 
+// GSAP Library
+let timeline = gsap.timeline();
+Array.from(parallax_el)
+.filter((el) => !el.classList.contains("text"))
+.forEach((el) => {
+        let timeline = gsap.timeline();
+        timeline.from(
+            el, 
+            {
+                top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+                duration: 3.5,
+                ease: "power3.out"
+            },
+        );
+    });
+
+timeline.from(
+    ".text h1", 
+    {
+        y: 
+            window.innerHeight - 
+            document.querySelector(".text h1").getBoundingClientRect().top + 200,
+        duration: 2,
+    },
+    "2.5"
+).from(
+    ".text h2", 
+    {
+        y: -150,
+        opacity: 0,
+        duration: 1.5,
+    },
+    "3"
+).from(
+    ".hide", 
+    {
+        opacity: 0,
+        duration: 1.5,
+    }, 
+    "3"
+);
